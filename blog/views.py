15a214c4660d8a516f3796108365719dddb55d5d
sbudisav <sbudisav@gmail.com>
2020-04-23 15:09:57 -0500

    adds views and forms

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
-  return render(request, 'blog/post_list.html', {})
+  posts = Post.objects.order_by('published_dt')
+  return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, 'blog/post_detail.html', {'post': post})
