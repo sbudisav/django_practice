4f4f680aa7f1b15bcfc837062ef30dfec6c33d9d
sbudisav <sbudisav@gmail.com>
2020-04-25 15:46:34 -0500

    revmoes product model

--- product/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Product(models.Model):
-  title = models.CharField(max_length=80)
-  description = models.TextField(blank=True, null=True)
-  price = models.DecimalField(max_digits=1000, decimal_places=2)
-  
