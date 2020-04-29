af92c3df5df2cf9bb4d13ddfde2305e892576008
sbudisav <sbudisav@gmail.com>
2020-04-28 21:35:33 -0500

    initial work on users

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 
-from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
 
 # Create your models here.
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "templates/register.html",
+                  context={"form":form})
