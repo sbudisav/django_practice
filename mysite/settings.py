a26c0436eb9cb664d46f566e8c5a5a29acb25835
sbudisav <sbudisav@gmail.com>
2020-04-28 20:27:03 -0500

    gets static file location working

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,10 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
     'static/',
 ]
-print("HERE IS STATIC ROOT")
 print(STATICFILES_DIRS)
